d00a0001a82211c39ca9659c4e44046b0d33c87c
allelbhagya <allel.bhagya@gmail.com>
2024-01-13 17:05:06 +0530

    table format

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -2,16 +2,47 @@
 import React from 'react';
 
 const Log = ({ _id, time, duration, region, sensorID, profile, stoppage, measure, comment, onDelete, onEdit }) => {
+  const removeUnwantedCharacters = (value) => value.replace(/[\\\"\'\`]/g, '');
+
+  const formatData = (data) => {
+    if (Array.isArray(data)) {
+      return data.map(item => {
+        if (Array.isArray(item)) {
+          return item.map(subItem => removeUnwantedCharacters(subItem).replace(/^\[|\]$/g, '')).join(', ');
+        } else if (typeof item === 'string') {
+          return removeUnwantedCharacters(item).replace(/^\[|\]$/g, '');
+        }
+        return item;
+      }).join(', ');
+    } else if (typeof data === 'string') {
+      return removeUnwantedCharacters(data).replace(/^\[|\]$/g, '');
+    }
+    return data;
+  };
+
+  // Format time or display original value if it's an invalid date
+  let formattedTime;
+  try {
+    formattedTime = new Date(time).toLocaleString();
+  } catch (error) {
+    console.error('Invalid date:', time);
+    formattedTime = time;
+  }
+
+  const formatRegion = formatData(region);
+  const formatSensorID = formatData(sensorID);
+  const formatStoppage = formatData(stoppage);
+
   return (
     <tr>
-      <td>{time}</td>
+      <td>{formattedTime}</td>
       <td>{duration}</td>
-      <td className="region-head">{region}</td>
-      <td>{Array.isArray(sensorID) ? sensorID.join(', ') : sensorID}</td> 
+      <td className="region-head">{formatRegion}</td>
+      <td>{formatSensorID}</td>
       <td>{profile}</td>
-      <td>{stoppage}</td>
-      <td>{measure}</td>
-      <td>{comment}</td>
+      <td>{formatStoppage}</td>
+      <td>{(measure)}</td>
+      <td>{(comment)}</td>
       <td>
         <div className='log-buttons'>
           <button onClick={() => onEdit(_id)}>Edit</button>
