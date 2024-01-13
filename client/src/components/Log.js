4dbe42d5c1e67809098b8914ab6fe7036bbc02ff
allelbhagya <allel.bhagya@gmail.com>
2024-01-13 14:22:28 +0530

    fixing sensor data on index page table

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -1,23 +1,22 @@
 // Log.js
 import React from 'react';
 
-const Log = ({ _id, time, duration, region, sensorId, profile, stoppage, measure, comment, onDelete, onEdit }) => {
+const Log = ({ _id, time, duration, region, sensorID, profile, stoppage, measure, comment, onDelete, onEdit }) => {
   return (
     <tr>
       <td>{time}</td>
       <td>{duration}</td>
       <td className="region-head">{region}</td>
-      <td>{sensorId}</td>
+      <td>{sensorID}</td> 
       <td>{profile}</td>
       <td>{stoppage}</td>
       <td>{measure}</td>
       <td>{comment}</td>
       <td>
         <div className='log-buttons'>
-        <button onClick={() => onEdit(_id)}>Edit</button>
-        <button onClick={() => onDelete(_id)}>Delete</button>
+          <button onClick={() => onEdit(_id)}>Edit</button>
+          <button onClick={() => onDelete(_id)}>Delete</button>
         </div>
-
       </td>
     </tr>
   );
